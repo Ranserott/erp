00a5848b1901356c2a4755f72e70df6316e62f91
ranserot <ranserot3000@gmail.com>
2025-11-21 22:56:29 -0300

    Corregir formato de env() con valores por defecto null directos

--- config/cache.php
+++ config/cache.php
@@ -41,9 +41,9 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'connection' => env('DB_CACHE_CONNECTION') ?: null,
+            'connection' => env('DB_CACHE_CONNECTION', null),
             'table' => env('DB_CACHE_TABLE', 'cache'),
-            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION') ?: null,
+            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION', null),
             'lock_table' => env('DB_CACHE_LOCK_TABLE', 'cache_locks'),
         ],
 
@@ -55,10 +55,10 @@ return [
 
         'memcached' => [
             'driver' => 'memcached',
-            'persistent_id' => env('MEMCACHED_PERSISTENT_ID') ?: null,
+            'persistent_id' => env('MEMCACHED_PERSISTENT_ID', null),
             'sasl' => [
-                env('MEMCACHED_USERNAME') ?: null,
-                env('MEMCACHED_PASSWORD') ?: null,
+                env('MEMCACHED_USERNAME', null),
+                env('MEMCACHED_PASSWORD', null),
             ],
             'options' => [
                 // Memcached::OPT_CONNECT_TIMEOUT => 2000,
@@ -80,11 +80,11 @@ return [
 
         'dynamodb' => [
             'driver' => 'dynamodb',
-            'key' => env('AWS_ACCESS_KEY_ID') ?: null,
-            'secret' => env('AWS_SECRET_ACCESS_KEY') ?: null,
+            'key' => env('AWS_ACCESS_KEY_ID', null),
+            'secret' => env('AWS_SECRET_ACCESS_KEY', null),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
             'table' => env('DYNAMODB_CACHE_TABLE', 'cache'),
-            'endpoint' => env('DYNAMODB_ENDPOINT') ?: null,
+            'endpoint' => env('DYNAMODB_ENDPOINT', null),
         ],
 
         'octane' => [
--- config/mail.php
+++ config/mail.php
@@ -39,12 +39,12 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME') ?: null,
-            'url' => env('MAIL_URL') ?: null,
+            'scheme' => env('MAIL_SCHEME', null),
+            'url' => env('MAIL_URL', null),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME') ?: null,
-            'password' => env('MAIL_PASSWORD') ?: null,
+            'username' => env('MAIL_USERNAME', null),
+            'password' => env('MAIL_PASSWORD', null),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
